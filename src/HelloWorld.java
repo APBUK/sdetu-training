9b812ab662d52cd7dee263f30341fbcb1ecb2cbf
APBUK <adamboweruk@gmail.com>
2018-03-08 16:06:17 -0600

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,9 +3,14 @@ import oop.ConTest2;
 public class HelloWorld extends ConTest2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("Adding more code");
 		
+		System.out.println("Hello World!");
+		System.out.println("Adding more code");	
+	}
+	
+	public int addTogether(int a, int b) {
+		
+		return a + b;
 	}
 }
 
